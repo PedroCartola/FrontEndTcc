5012d0db2463d0afa2425a0d2d8e0af5a7ba18bd
Pedro Henrique Almeida Trivelato <ra2357056>
2025-04-15 09:06:15 -0300

    Bom dia 2

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -5,6 +5,7 @@ import {
     View,
     Image,
     TextInput,
+    Button,
 } from 'react-native';
 import { style } from "./style";
 import Logo from "../../assets/logo.png";
@@ -13,14 +14,29 @@ export default function Login(){
     return(
         <View style={style.conteiner}>
             <View style={style.boxTop}>
-                <Text>Bem Vindo</Text>
-                <TextInput placeholder="E-mail" style={style.inputText}/>
-                <TextInput placeholder="Senha" style={style.inputText}/>
-                <TextInput placeholder="Confirme sua senha" style={style.inputText}/>
+                <Text style={style.criarConta}>Criar conta</Text>
+                <Text>Start your 30 day free trial. Cancel any time</Text>
+                <View style={style.buttonLogin}>
+                    <Button title='Google'/>
+                    <Button title="Facebook" />
+                </View>
+                <Text>===================or===================</Text>
+                <TextInput placeholder="E-mail" style={style.inputText} />
+                <TextInput placeholder="Senha" style={style.inputText} />
+                <TextInput placeholder="Confirme sua senha" style={style.inputText} />
+                <Text>Esqueseu a senha?</Text>
+                <Text>Check box</Text>
+                <View style={style.boxBotton}>
+                    <View style={style.buttonLogin}>
+                        <Button title="Criar conta"></Button>
+                    </View>
+                </View>
+                <Text>Já tem uma conta? <>Login in</></Text>
             </View>
-            <View style={style.boxBotton}>
-                <Text>button</Text>
-            </View>
+                <Image
+                source={Logo}
+                style={style.image}
+                />
         </View>
     )
 }
--- src/pages/login/style.ts
+++ src/pages/login/style.ts
@@ -3,27 +3,45 @@ import { Dimensions, StyleSheet } from "react-native";
 export const style = StyleSheet.create({
     conteiner:{
         flex: 1,
+        width: '100%',
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#0BD979',
     },
+        image:{
+            width: 70,
+            height: 70,
+        },
     boxTop:{
-        height: Dimensions.get('window').height/1.5,
+        flex: 1,
+        height: 100,
         width: '100%',
         backgroundColor: 'white',
         alignItems: 'center',
         justifyContent: 'center',
+        borderEndStartRadius: 100,
+        borderEndEndRadius: 100,
     },
+        buttonLogin:{
+            width: '80%',
+            height: 80,
+            margin: 10
+        },
         inputText:{
+            width: '80%',
+            height: 40,
             borderWidth: 1,
             borderRadius: 4,
             color: 'gray',
-            margin:10
+            margin: 10
+        },
+        criarConta:{
+            fontFamily: 'arial',
+            fontSize: 25,
+            color: '#09B565',
         },
     boxBotton:{
-        height: Dimensions.get('window').height/4,
         width: '100%',
-        backgroundColor: 'white',  
         alignItems: 'center',
         justifyContent: 'center',
     },
